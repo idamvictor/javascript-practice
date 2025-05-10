d512d02062dbd883e9877b32214faa7024e58719
Victor Idam <idamvictorx1@gmail.com>
2025-05-10 23:21:21 +0100

    firstcommit

---
+++ script.js
@@ -0,0 +1,7 @@
+// function to say hello world
+function sayWord(word, meaning) {
+  return word + meaning;
+}
+
+let couple_words = sayWord(" Hello world! ", "it is welcoming the people");
+console.log(couple_words)
