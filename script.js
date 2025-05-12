727f496c425a3df24a41fd91cdbcc71bdb03899f
Victor Idam <idamvictorx1@gmail.com>
2025-05-12 22:22:35 +0100

    array practice

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function sayWord(word, meaning) {
 
 let couple_words = sayWord(" Hello world! ", "it is welcoming the people");
 console.log(couple_words)
+
+// Arrays
+let myFirstArray = [1, 2, 3, 4, 5]
+console.log(myFirstArray[1])
