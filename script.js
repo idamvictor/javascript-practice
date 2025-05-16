c036f3f84de5c7f0e3938f131d118bdfc8490498
Victor Idam <idamvictorx1@gmail.com>
2025-05-16 10:03:41 +0100

    working with variables

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-// function to say hello world
-function sayWord(word, meaning) {
-  return word + meaning;
-}
-
-let couple_words = sayWord(" Hello world! ", "it is welcoming the people");
-console.log(couple_words)
-
-// Arrays
-let myFirstArray = [1, 2, 3, 4, 5]
-console.log(myFirstArray[1])
+var age = null;
+console.log(age);
+console.log(typeof age)
+var age = 25;
+console.log(age);
